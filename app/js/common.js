921db46c62f6fc6056804b531c90bf89313c5c72
StarkOne <Stark.pro40@gmail.com>
2017-04-14 19:40:15 +0700

    section_button

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
 $(function() {
 
-	// Custom JS
+	$(".js-dark-title").on("click", function(e){
+				e.preventDefault();
+				var $this = $(this),
+						answerId = $this.attr("href");
+				if(!$this.hasClass("active")){
+						$(".js-dark-content").slideUp();
+						$(".js-dark-title").removeClass("active");
+				}
+				$this.toggleClass("active");
+				$(answerId).slideToggle();
+		}); 
 
 });
